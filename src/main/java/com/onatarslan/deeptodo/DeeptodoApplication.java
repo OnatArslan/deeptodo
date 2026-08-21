b5a12d616e106f42f83686a1ae4d2d8876573769
OnatArslan <onatarslan13@gmail.com>
2026-08-22 02:23:10 +0300

    initial commit

---
+++ src/main/java/com/onatarslan/deeptodo/DeeptodoApplication.java
@@ -0,0 +1,13 @@
+package com.onatarslan.deeptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeptodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeptodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onatarslan/deeptodo/DeeptodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onatarslan.deeptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeptodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
